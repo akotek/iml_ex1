93576199b8408e3da1cd12dd7cc5bf4c8c543a26
kotek <kotekmath@gmail.com>
2018-04-12 16:30:15 +0300

    step 2

--- ex1.py
+++ ex1.py
@@ -2,71 +2,100 @@ from numpy import diag, dot
 from numpy.linalg import svd, norm, matrix_rank
 import matplotlib.pyplot as plt
 import scipy.misc as misc
-
-a = np.array([[1, 1, 1, 0, 0],
-              [3, 3, 3, 0, 0],
-              [4, 4, 4, 0, 0],
-              [5, 5, 5, 0, 0],
-              [0, 2, 0, 4, 4],
-              [0, 0, 0, 5, 5],
-              [0, 1, 0, 2, 2]])
-
-# -------- INIT STUFF -------------
-np.set_printoptions(suppress=True)
-np.set_printoptions(precision=3)
-
-img = misc.ascent()
-print("--- FULL ---")
-U, S, VT = svd(a, full_matrices=False)
-print("U:\n {}".format(U))
-print("s:\n {}".format(S))
-print("VT:\n {}".format(VT))
-
-
-# --------------------------------
+import copy
 
 
 def zero_k_sing_values(s, k):
-    # TODO delete k
-    if s is None or k is None or k < 0:
-        raise ValueError("bad input")
-
     for sing_val in range(k, len(s)):
         s[sing_val] = 0
 
     return s
 
 
-def reconstruct_rank_k(u, s, vt, k):
-    # TODO delete k
-    if u is None or s is None or vt is None or k < 0:
-        raise ValueError("bad input")
-
-    sing_mat_after_zero_k = zero_k_sing_values(s, k)
-    return dot(
-        dot(u, diag(sing_mat_after_zero_k)),
-        vt)
+def reconstruct_rank_k_mat(u, s, vt, k):
+    zerod_s = zero_k_sing_values(s, k)
+    us_mat = dot(u, diag(zerod_s))
+    return dot(us_mat, vt)
 
 
 def calc_compression_ratio(square_mat, k):
     r = matrix_rank(square_mat)  # r=rank
     n = len(square_mat)
-    return 1 - \
-           ((2 * k * n) + k) / ((2 * n * r) + n)
+    return 1 - ((2 * k * n) + k) / ((2 * n * r) + n)
 
 
 def forbenius_dist(mat1, mat2):
     return norm(mat1 - mat2)
 
+
+def get_rec_forb_comp(img, u, s, vt, k):
+    if img is None or u is None or s is None or vt is None:
+        raise ValueError("bad input")
+
+    rec_matrix = reconstruct_rank_k_mat(u, s, vt, k)
+    y1_forb_dist = (forbenius_dist(img, rec_matrix))
+    y2_comp_ratio = (calc_compression_ratio(img, k))
+
+    return rec_matrix, y1_forb_dist, y2_comp_ratio
+
+
+def plot_graph(x, title_x, y, title_y):
+    plt.title('Plotting {0} and {1}'.format(title_x, title_y))
+    plt.xlabel(title_x)
+    plt.ylabel(title_y)
+    plt.plot(x, y)
+    plt.show()
+    return
+
+
+def plot_images_from_list(image_list, k_list, forb_dist, comp_ratio):
+
+    fig, axeslist = plt.subplots(ncols=3, nrows=2)
+    fig.suptitle("5 Images Plot", fontsize=15)
+
+    for i in range(len(image_list)):
+        axeslist.ravel()[i].imshow(image_list[i], 'gray')
+        axeslist.ravel()[i].set_title(
+            "K: {0},\n Forb Dist: {1:.1f}, Comp Ratio: {2:.1f}".format(
+                k_list[i],forb_dist[i], comp_ratio[i]), fontsize=10)
+        axeslist.ravel()[i].set_axis_off()
+    plt.show()
+
+
 def main():
-    x = 5
-    y = 6
 
-    # [forb1, forb2, forb2,......forbK]
-    #
+    # ---- Init data ----------
+    img = misc.ascent()
+    U, S, VT = svd(img, full_matrices=False)
+    # -------------------------
 
-# print(diag(S))
-# print(diag(zero_k_sing_values(S,1)))
-# print(a)
-# print(reconstruct_rank_k(U,S,VT,2))
-if  __name__ =='__main__':main()
+    # -------- 1st Plot --------
+    k_x_axis = []
+    y1_forb_dist = []
+    y2_comp_ratio = []
+    for k in range(len(img)):
+        k_x_axis.append(k)
+        rec_img, y1, y2 = get_rec_forb_comp(img, U, copy.deepcopy(S), VT, k)
+        y1_forb_dist.append(y1)
+        y2_comp_ratio.append(y2)
+
+    plot_graph(k_x_axis, "k", y1_forb_dist, "Forb Dist")
+    plot_graph(k_x_axis, "k", y2_comp_ratio, "Compression Ratio")
+    # -------------------------
+
+    # # -------- 2nd Plot --------
+    k_image_list = [5, 20, 45, 250, 511]
+    image_list = []
+    y1_forb_dist = []
+    y2_comp_ratio = []
+    for k in k_image_list:
+        rec_img, y1, y2 = get_rec_forb_comp(img, U, copy.deepcopy(S),VT, k)
+        image_list.append(rec_img)
+        y1_forb_dist.append(y1)
+        y2_comp_ratio.append(y2)
+
+    plot_images_from_list(image_list, k_image_list, y1_forb_dist,y2_comp_ratio)
+    # -------------------------
+
+if __name__ == '__main__': main()
+
